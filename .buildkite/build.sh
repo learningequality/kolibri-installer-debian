f881ea77c440b16ef3b3c538d3d5d87e6785668d
David Canas <david@learningequality.org>
2020-04-09 21:06:29 -0700

    Modifying pipeline to include src.

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -2,12 +2,13 @@
 
 set -eo pipefail
 
+mkdir src
+
 if [[ $LE_TRIGGERED_FROM_BUILD_ID ]]
 then
   echo "--- Downloading from triggered build"
   buildkite-agent artifact download "dist/*.tar.gz" . --build $LE_TRIGGERED_FROM_BUILD_ID
-
-
+  mv dist src
   if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID ]]
   then
     buildkite-agent annotate "This is a rebuild from a triggered build. Using parent's tarball" --style warning
