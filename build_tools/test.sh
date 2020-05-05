a2052cffa8596d9fc1437e9b65ddf589b8f874e6
David Canas <david@learningequality.org>
2020-05-05 16:04:43 -0700

    Preseeding Kolibri installation values. Copying approach here: https://gitlab.com/bibliosansfrontieres/olip/kolibri/-/tree/b92f0327e8527a98ba1c8ed745da24a8903ff633
    Co-Authored-By: José L. Redrejo Rodríguez <jredrejo@debian.org>

--- build_tools/test.sh
+++ build_tools/test.sh
@@ -5,8 +5,10 @@ DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 apt-get install -y python3-pkg-resources
 
-adduser --disabled-login --gecos "" kolibri
-
+# Using debconf-set-selections requires that it be an interactive shell
+unset DEBIAN_FRONTEND
+debconf-set-selections $BOUND_DIR/build_tools/preseed.cfg
 dpkg -i $BOUND_DIR/dist/*.deb
 
-su kolibri -p -c 'kolibri start ; kolibri stop'
+kolibri start
+kolibri stop
