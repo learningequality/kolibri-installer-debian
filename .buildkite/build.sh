96e0724a3a6a6f9fd17175ab33321f8478f08bb0
David Canas <david@learningequality.org>
2020-06-19 14:05:55 -0700

    Adjusting mv syntax to work with busybox implementation.

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -8,7 +8,7 @@ if [[ $LE_TRIGGERED_FROM_BUILD_ID ]]
 then
   echo "--- Downloading from triggered build"
   buildkite-agent artifact download "dist/*.tar.gz" . --build $LE_TRIGGERED_FROM_BUILD_ID
-  mv dist build_src
+  mv dist/*.tar.gz build_src/
   if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID ]]
   then
     buildkite-agent annotate "This is a rebuild from a triggered build. Using parent's tarball" --style warning
