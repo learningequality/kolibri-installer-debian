650f60b71e7ed95904c30f034270bb58533e045c
David Canas <david@learningequality.org>
2020-06-25 13:04:46 -0700

    Bringing back the focal test, and adding new dep to tests Reported by Lingyi to fix the problems associated with
    Co-Authored-By: Lingyi Wang <lyw07@users.noreply.github.com>

--- build_tools/test.sh
+++ build_tools/test.sh
@@ -11,7 +11,7 @@ export LANG=C.UTF-8
 
 
 apt-get -y update
-apt-get install -y python3-pkg-resources
+apt-get install -y iproute2 python3-pkg-resources
 
 # BOUND_DIR set in build_tools/docker_test.sh
 debconf-set-selections $BOUND_DIR/build_tools/preseed.cfg
