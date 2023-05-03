7ba3ac0242c5fec4bb153d7ca04b793dc064df61
José Redrejo <jose@learningequality.org>
2023-05-03 09:13:40 +0200

    Signing sources from GHA

--- build_tools/build.sh
+++ build_tools/build.sh
@@ -1,11 +1,34 @@
 #! /usr/bin/env bash
 
-set -eo pipefail
+set -exo pipefail
 # set -euxo pipefail
 # removed -u because it was causing an error when reading VIRTUAL_ENV from the environment
 
 # When this script is run with -S, it builds a source package only.
 # It will ask to sign the package if the $DEBMAIL gpg key is installed in the system
+
+
+signfile () {
+
+    local file="$1"
+    if [[ "$GPG_PASSPHRASE" != "" ]]
+    then
+        UNSIGNED_FILE=../"$(basename "$file")"
+        ASCII_SIGNED_FILE="${UNSIGNED_FILE}.asc"
+
+        gpg --utf8-strings  --clearsign --armor --textmode --batch --pinentry loopback --passphrase "$GPG_PASSPHRASE" --weak-digest SHA1 --weak-digest RIPEMD160 --output "$ASCII_SIGNED_FILE" "$UNSIGNED_FILE"
+        mv -f "$ASCII_SIGNED_FILE" "$UNSIGNED_FILE"
+    fi
+
+}
+
+signfiles(){
+    local file="$1"
+    signfile "$file".dsc
+    signfile "$file"_source.buildinfo
+    signfile "$file"_source.changes
+}
+
 
 BUILD_BINARY=1
 
@@ -26,8 +49,8 @@ cp -r ../debian $SOURCE_DIR
 DEB_VERSION=`cat VERSION | sed -s 's/^\+\.\+\.\+\([abc]\|\.dev\)/\~\0/g'`
 
 # use the environment variables if they are set:
-DEBFULLNAME="${DEBFULLNAME:-Learning Equality}"
-DEBEMAIL="${DEBEMAIL:-info@learningequality.org}"
+DEBFULLNAME="${DEBFULLNAME:-Learning Equality \(Learning Equality\'s public signing key\)}"
+DEBEMAIL="${DEBEMAIL:-accounts@learningequality.org>}"
 
 cd $SOURCE_DIR
 DEBFULLNAME=$DEBFULLNAME DEBEMAIL=$DEBEMAIL dch -b -v $DEB_VERSION-0ubuntu1 'New upstream release'
@@ -42,7 +65,8 @@ fi
 
 if [[ "$BUILD_BINARY" -eq "0" ]]; then
     # build source package only
-   dpkg-buildpackage -S || { echo "Not signing the sources: GPG secret key for $DEBEMAIL is not available" ; }
+   dpkg-buildpackage -S  --no-sign
+   signfiles kolibri-source_$DEB_VERSION-0ubuntu1
 else
     # build with unsigned source, changes and gzip compression
     dpkg-buildpackage -A -Zgzip -z3 -us -uc
